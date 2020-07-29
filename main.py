1b68612fcc9fafb8a8c97a460cc21d558800a021
Suhas H E <t-suh@microsoft.com>
2020-07-29 16:42:16 +0530

    Added cors

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from flask import Flask, request
+from flask_cors import CORS
 import json
 import os
 
+from Helpers.LocalInfo import getLocalInfo
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def main_func():
   return 'Hello Flask, on Azure App Service for Linux!'
 
-@app.route('/local_stores',methods=['GET'])
+@app.route('/local_info',methods=['GET'])
 def get_local():
-  point = 12.981480, 77.507180
+
+  lattitude = request.args.get('lattitude')
+  longitude = request.args.get('longitude')
+  entityType = request.args.get('entityType')
+
+  response = getLocalInfo(entityType,lattitude,longitude)
+  return response
 
 if __name__ == '__main__':                 
   app.run(host="0.0.0.0", port=5001, debug=True)
